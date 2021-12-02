9b54be1719171e64eff72518d498303895173af9
junbin.yang <282764407@qq.com>
2021-12-02 19:07:14 +0800

    add

--- router.go
+++ router.go
@@ -26,10 +26,8 @@ func newRoute() *route {
 func (r *route) addRoute(method string, pattern string, handler []HandlerFunc) {
 	parts := parsePattern(pattern)
 	if _, ok := r.roots[method]; !ok {
-		//r.roots[method] = &node{}
 		r.roots[method] = &node{children: make(map[string]*node)}
 	}
-	//r.roots[method].insert(pattern, parts, 0)
 	r.roots[method].insert(pattern, parts)
 
 	key := method + "-" + pattern
@@ -40,7 +38,6 @@ func (r *route) addRoute(method string, pattern string, handler []HandlerFunc) {
 // 例如/p/go/doc匹配到/p/:lang/doc，解析结果为：{lang: "go"}，/static/css/h.css匹配到/static/*filepath，解析结果为{filepath: "css/h.css"}。
 func (r *route) getRoute(method string, path string) (*node, []Param) {
 	searchParts := parsePattern(path)
-	// var params []Param
 	root := r.roots[method]
 	if root == nil {
 		return nil, nil
@@ -48,23 +45,6 @@ func (r *route) getRoute(method string, path string) (*node, []Param) {
 
 	// 在该方法的路由树上查找该路径
 	return root.search(searchParts)
-	/*
-		n := root.search(searchParts, 0)
-		if n != nil {
-			parts := parsePattern(n.pattern)
-			for index, part := range parts {
-				if part[0] == ':' {
-					params = append(params, Param{part[1:], searchParts[index]})
-				}
-				if part[0] == '*' && len(part) > 1 {
-					params = append(params, Param{part[1:], strings.Join(searchParts[index:], "/")})
-					break
-				}
-			}
-			return n, params
-		}
-		return nil, nil
-	*/
 }
 
 // 找到并执行处理请求函数
--- trie.go
+++ trie.go
@@ -6,9 +6,8 @@ import (
 
 // 前缀树实现
 type node struct {
-	pattern string // 是否一个完整的url，不是则为空字符串
-	part    string // URL块值，用/分割的部分，比如/abc/123，abc和123就是2个part
-	//children []*node // 节点下的子节点
+	pattern  string           // 是否一个完整的url，不是则为空字符串
+	part     string           // URL块值，用/分割的部分，比如/abc/123，abc和123就是2个part
 	children map[string]*node // 节点下的子节点
 	isWild   bool             // 是否模糊匹配，比如:id*id这样的node就为true
 }
@@ -38,54 +37,6 @@ func (n *node) insert(pattern string, parts []string) {
 	this.pattern = pattern
 }
 
-/*
-func (n *node) insert(pattern string, parts []string, height int) {
-	if len(parts) == height {
-		// 如果已经匹配完了，那么将pattern赋值给该node，表示它是一个完整的url
-		// 这是递归的终止条件
-		n.pattern = pattern
-		return
-	}
-
-	part := parts[height]
-	child := n.matchChild(part)
-	if child == nil {
-		// 没有匹配上，那就初始化一个，放到n节点的子列表中
-		child = &node{part: part, isWild: part[0] == ':' || part[0] == '*'}
-		n.children = append(n.children, child)
-	}
-	// 接着插入下一个part节点
-	child.insert(pattern, parts, height+1)
-}
-
-
-func (n *node) search(parts []string, height int) *node {
-	if len(parts) == height || strings.HasPrefix(n.part, "*") {
-		// 递归终止条件，找到末尾了或者通配符
-		if n.pattern == "" {
-			// pattern为空字符串表示它不是一个完整的url，匹配失败
-			return nil
-		}
-		return n
-	}
-
-	part := parts[height]
-	// 获取所有可能的子路径
-	children := n.matchChildren(part)
-
-	for _, child := range children {
-		// 对于每条路径接着用下一part去查找
-		result := child.search(parts, height+1)
-		if result != nil {
-			// 找到了即返回
-			return result
-		}
-	}
-
-	return nil
-}
-*/
-
 func (n *node) search(parts []string) (*node, []Param) {
 	var params []Param
 	this := n
@@ -112,42 +63,3 @@ func (n *node) search(parts []string) (*node, []Param) {
 	}
 	return this, params
 }
-
-/*
-// 查找匹配的子节点，场景是用在插入时使用，找到1个匹配的就立即返回
-func (n *node) matchChild(part string) *node {
-	// 遍历n节点的所有子节点
-	for _, child := range n.children {
-		//if child.isWild {
-		//	panic(part + "路由冲突，同级已经有" + child.part)
-		//}
-		//if child.part == part {
-		//	return child
-		//}
-
-		// 动态匹配做强校验,防止路由注册覆盖
-		if child.part == part || ((part[0] == ':' || part[0] == '*') && child.isWild) {
-			return child
-		}
-
-	}
-	return nil
-}
-
-
-// 查找所有匹配成功的节点
-func (n *node) matchChildren(part string) []*node {
-	nodes := make([]*node, 0)
-	wildNodes := make([]*node, 0)
-	for _, child := range n.children {
-		// 静态路由节点优先,动态路由节点延后
-		if child.part == part {
-			nodes = append(nodes, child)
-		} else if child.isWild {
-			wildNodes = append(wildNodes, child)
-		}
-	}
-	nodes = append(nodes, wildNodes...)
-	return nodes
-}
-*/
